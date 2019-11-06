206a1b58366632b8fc9c69193a1809868a405ce9
Francis Li <mail@francisli.com>
2019-11-05 18:47:50 -0800

    Added a POST form and route

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,14 @@ router.get('/school', function(req, res, next) {
   });
 });
 
+router.post('/school2', function(req, res, next) {
+  console.log(req.body);
+  res.render('school', {
+    name: req.body.name,
+    lastname: req.body.lastname
+  });
+});
+
 router.get('/logout', function(req,res,next){
   req.logout();
   req.flash('info', 'You have been logged out.');
