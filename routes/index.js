6aa9bb7a7990e379588630307d93352083325655
Francis Li <mail@francisli.com>
2020-01-14 17:38:42 -0800

    Edit and delete pages

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,42 @@ router.post('/new-post', function(req, res, next) {
   });
 });
 
+router.get('/edit-post', function(req, res, next) {
+  models.Post.findByPk(req.query.id).then(function(record) {
+    res.render('edit-post', {
+      record: record
+    });
+  });
+})
+
+router.post('/edit-post', function(req, res, next) {
+  models.Post.findByPk(req.query.id).then(function(record) {
+    record.update({
+      from: req.body.from,
+      title: req.body.title,
+      full_text: req.body.full_text
+    }).then(function() {
+      res.redirect(`/posts?id=${record.id}`);
+    })
+  });
+});
+
+router.get('/delete-post', function(req, res, next) {
+  models.Post.findByPk(req.query.id).then(function(record) {
+    res.render('delete-post', {
+      record: record
+    });
+  });
+})
+
+router.post('/delete-post', function(req, res, next) {
+  models.Post.findByPk(req.query.id).then(function(record) {
+    record.destroy().then(function() {
+      res.redirect('/');
+    });
+  });
+});
+
 router.get('/school', function(req, res, next) {
   console.log(req.query);
   res.render('school', {
