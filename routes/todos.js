30f2350f7ae03ec4ed6ef34414c943279c872242
Francis Li <mail@francisli.com>
2020-01-28 18:56:34 -0800

    Edit/update routes

--- routes/todos.js
+++ routes/todos.js
@@ -25,4 +25,23 @@ router.post('/', function(req, res, next) {
   });
 });
 
+router.get('/:id', function(req, res, next) {
+  models.Todo.findByPk(req.params.id).then(function(record) {
+    res.render('todo/edit', {
+      record: record
+    });
+  });
+});
+
+router.post('/:id', function(req, res, next) {
+  models.Todo.findByPk(req.params.id).then(function(record) {
+    record.update({
+      title: req.body.title,
+      body: req.body.body
+    }).then(function(record) {
+      res.redirect('/todo');
+    })
+  })
+});
+
 module.exports = router;
